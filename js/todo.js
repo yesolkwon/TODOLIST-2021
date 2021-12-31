bdaa3974b5d63a86809882fab5bd53bd134531df
Yesol Kwon <kwonyesol@gmail.com>
2021-12-31 19:12:21 +1000

    4

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector("#todo-list");
 
+const TODOS_KEY = "todos";
+let toDos = [];
+
+function saveTodo() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function handleTodolist(event) {
   event.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = "";
+  toDos.push(newTodo);
   paintTodo(newTodo);
+  saveTodo();
 }
 
 function deleteTodo(event) {
@@ -26,4 +35,13 @@ function paintTodo(addtodo) {
   li.appendChild(span);
   li.appendChild(button);
 }
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if (savedTodos !== null) {
+  const parsedTodo = JSON.parse(savedTodos);
+  toDos = parsedTodo;
+  parsedTodo.forEach(paintTodo);
+}
+
 todoForm.addEventListener("submit", handleTodolist);
